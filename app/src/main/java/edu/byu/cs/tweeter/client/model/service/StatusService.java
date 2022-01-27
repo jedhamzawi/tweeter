ddb28f0c1a32c0aceda983570ebf687bd82b3950
hamsesh <hamsesh@protonmail.ch>
2022-01-26 22:24:24 -0700

    Post status

--- app/src/main/java/edu/byu/cs/tweeter/client/model/service/StatusService.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/model/service/StatusService.java
@@ -1,4 +1,126 @@
 package edu.byu.cs.tweeter.client.model.service;
 
+import android.os.Handler;
+import android.os.Message;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import java.net.MalformedURLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import edu.byu.cs.tweeter.client.cache.Cache;
+import edu.byu.cs.tweeter.client.model.service.backgroundTask.PostStatusTask;
+import edu.byu.cs.tweeter.client.view.main.MainActivity;
+import edu.byu.cs.tweeter.model.domain.Status;
+import edu.byu.cs.tweeter.model.domain.User;
+
 public class StatusService {
+
+    public interface PostStatusObserver {
+        void handleSuccess();
+        void handleFailure(String message);
+        void handleException(Exception ex);
+    }
+
+    public void postStatus(String post, User currUser, PostStatusObserver observer) throws ParseException, MalformedURLException {
+        Status newStatus = new Status(post, currUser, getFormattedDateTime(), parseURLs(post), parseMentions(post));
+        PostStatusTask statusTask = new PostStatusTask(Cache.getInstance().getCurrUserAuthToken(),
+                newStatus, new PostStatusHandler(observer));
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.execute(statusTask);
+    }
+
+    private class PostStatusHandler extends Handler {
+        private PostStatusObserver observer;
+
+        public PostStatusHandler(PostStatusObserver observer) {
+            this.observer = observer;
+        }
+
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            boolean success = msg.getData().getBoolean(PostStatusTask.SUCCESS_KEY);
+            if (success) {
+                observer.handleSuccess();
+            } else if (msg.getData().containsKey(PostStatusTask.MESSAGE_KEY)) {
+                String message = msg.getData().getString(PostStatusTask.MESSAGE_KEY);
+                observer.handleFailure(message);
+            } else if (msg.getData().containsKey(PostStatusTask.EXCEPTION_KEY)) {
+                Exception ex = (Exception) msg.getData().getSerializable(PostStatusTask.EXCEPTION_KEY);
+                observer.handleException(ex);
+            }
+        }
+    }
+
+    private List<String> parseURLs(String post) throws MalformedURLException {
+        List<String> containedUrls = new ArrayList<>();
+        for (String word : post.split("\\s")) {
+            if (word.startsWith("http://") || word.startsWith("https://")) {
+
+                int index = findUrlEndIndex(word);
+
+                word = word.substring(0, index);
+
+                containedUrls.add(word);
+            }
+        }
+
+        return containedUrls;
+    }
+
+    private int findUrlEndIndex(String word) {
+        if (word.contains(".com")) {
+            int index = word.indexOf(".com");
+            index += 4;
+            return index;
+        } else if (word.contains(".org")) {
+            int index = word.indexOf(".org");
+            index += 4;
+            return index;
+        } else if (word.contains(".edu")) {
+            int index = word.indexOf(".edu");
+            index += 4;
+            return index;
+        } else if (word.contains(".net")) {
+            int index = word.indexOf(".net");
+            index += 4;
+            return index;
+        } else if (word.contains(".mil")) {
+            int index = word.indexOf(".mil");
+            index += 4;
+            return index;
+        } else {
+            return word.length();
+        }
+    }
+
+    private String getFormattedDateTime() throws ParseException {
+        SimpleDateFormat userFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        SimpleDateFormat statusFormat = new SimpleDateFormat("MMM d yyyy h:mm aaa");
+
+        return statusFormat.format(userFormat.parse(LocalDate.now().toString() + " " + LocalTime.now().toString().substring(0, 8)));
+    }
+
+    private List<String> parseMentions(String post) {
+        List<String> containedMentions = new ArrayList<>();
+
+        for (String word : post.split("\\s")) {
+            if (word.startsWith("@")) {
+                word = word.replaceAll("[^a-zA-Z0-9]", "");
+                word = "@".concat(word);
+
+                containedMentions.add(word);
+            }
+        }
+
+        return containedMentions;
+    }
 }
--- app/src/main/java/edu/byu/cs/tweeter/client/presenter/MainPresenter.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/presenter/MainPresenter.java
@@ -1,32 +1,42 @@
 package edu.byu.cs.tweeter.client.presenter;
 
 
+import android.widget.Toast;
+
+import java.net.MalformedURLException;
+import java.text.ParseException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import edu.byu.cs.tweeter.client.cache.Cache;
 import edu.byu.cs.tweeter.client.model.service.FollowService;
+import edu.byu.cs.tweeter.client.model.service.StatusService;
 import edu.byu.cs.tweeter.client.model.service.UserService;
 import edu.byu.cs.tweeter.client.model.service.backgroundTask.LogoutTask;
+import edu.byu.cs.tweeter.client.view.main.MainActivity;
 import edu.byu.cs.tweeter.model.domain.User;
 
 public class MainPresenter {
+
     public interface View {
         void displayMessage(String message);
         void updateFollower(boolean isFollower);
         void updateFollow(boolean follow);
         void updateUnfollow(boolean unfollow);
         void logoutUser();
+        void cancelPostingTost();
     }
 
     private final View view;
     private FollowService followService;
     private UserService userService;
+    private StatusService statusService;
 
     public MainPresenter(View view) {
         this.view = view;
         followService = new FollowService();
         userService = new UserService();
+        statusService = new StatusService();
     }
 
     public void isFollower(User selectedUser) {
@@ -46,6 +56,11 @@ public class MainPresenter {
 
     public void logoutUser() {
         userService.logoutUser(Cache.getInstance().getCurrUserAuthToken(), new LogoutObserver());
+    }
+
+    public void postStatus(String post) throws ParseException, MalformedURLException {
+        statusService.postStatus(post, Cache.getInstance().getCurrUser(), new PostStatusObserver());
+
     }
 
     public class IsFollowerObserver implements FollowService.IsFollowerObserver {
@@ -112,12 +127,30 @@ public class MainPresenter {
 
         @Override
         public void handleFailure(String message) {
-
+            view.displayMessage("Failed to logout: " + message);
         }
 
         @Override
         public void handleException(Exception ex) {
+            view.displayMessage("Failed to logout because of exception: " + ex.getMessage());
+        }
+    }
 
+    public class PostStatusObserver implements StatusService.PostStatusObserver {
+
+        @Override
+        public void handleSuccess() {
+            view.cancelPostingTost();
+        }
+
+        @Override
+        public void handleFailure(String message) {
+            view.displayMessage("Failed to post status: " + message);
+        }
+
+        @Override
+        public void handleException(Exception ex) {
+            view.displayMessage("Failed to post status because of exception: " + ex.getMessage());
         }
     }
 }
