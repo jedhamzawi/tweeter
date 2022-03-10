2ddc443081e51aace6c55138e7dba8873e22a5e1
hamsesh <hamsesh@protonmail..ch>
2022-03-10 20:05:03 +0100

    Milestone #3 - Register with AWS

--- app/src/main/java/edu/byu/cs/tweeter/client/model/service/backgroundTask/RegisterTask.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/model/service/backgroundTask/RegisterTask.java
@@ -1,15 +1,26 @@
 package edu.byu.cs.tweeter.client.model.service.backgroundTask;
 
 import android.os.Handler;
+import android.util.Log;
+
+import java.io.IOException;
 
 import edu.byu.cs.tweeter.model.domain.AuthToken;
 import edu.byu.cs.tweeter.model.domain.User;
+import edu.byu.cs.tweeter.model.net.TweeterRemoteException;
+import edu.byu.cs.tweeter.model.net.request.LoginRequest;
+import edu.byu.cs.tweeter.model.net.request.RegisterRequest;
+import edu.byu.cs.tweeter.model.net.response.LoginResponse;
+import edu.byu.cs.tweeter.model.net.response.RegisterResponse;
 import edu.byu.cs.tweeter.util.Pair;
 
 /**
  * Background task that creates a new user account and logs in the new user (i.e., starts a session).
  */
 public class RegisterTask extends AuthenticateTask {
+
+    private static final String LOG_TAG = "RegisterTask";
+    private static final String URL_PATH = "/register";
 
     /**
      * The user's first name.
@@ -35,9 +46,27 @@ public class RegisterTask extends AuthenticateTask {
     }
 
     @Override
+    protected void runTask() {
+        try {
+            RegisterResponse response = getServerFacade().register(new RegisterRequest(this.username, this.password, image), URL_PATH);
+            if (response.isSuccess()) {
+                this.authenticatedUser = response.getUser();
+                this.authToken = response.getAuthToken();
+                sendSuccessMessage();
+            }
+            else {
+                sendFailedMessage(response.getMessage());
+            }
+        }
+        catch (IOException | TweeterRemoteException e) {
+            Log.e(LOG_TAG, "Unable to register due to exception: " + e.getMessage());
+            sendExceptionMessage(e);
+        }
+    }
+
+    //TODO: Remove
+    @Override
     protected Pair<User, AuthToken> runAuthenticationTask() {
-        User registeredUser = getFakeData().getFirstUser();
-        AuthToken authToken = getFakeData().getAuthToken();
-        return new Pair<>(registeredUser, authToken);
+        return null;
     }
 }
--- server/src/main/java/edu/byu/cs/tweeter/server/service/UserService.java
+++ server/src/main/java/edu/byu/cs/tweeter/server/service/UserService.java
@@ -3,7 +3,9 @@ package edu.byu.cs.tweeter.server.service;
 import edu.byu.cs.tweeter.model.domain.AuthToken;
 import edu.byu.cs.tweeter.model.domain.User;
 import edu.byu.cs.tweeter.model.net.request.LoginRequest;
+import edu.byu.cs.tweeter.model.net.request.RegisterRequest;
 import edu.byu.cs.tweeter.model.net.response.LoginResponse;
+import edu.byu.cs.tweeter.model.net.response.RegisterResponse;
 import edu.byu.cs.tweeter.util.FakeData;
 
 public class UserService {
@@ -49,5 +51,18 @@ public class UserService {
      */
     FakeData getFakeData() {
         return new FakeData();
+    }
+
+    public RegisterResponse register(RegisterRequest request) {
+        if(request.getUsername() == null){
+            throw new RuntimeException("[BadRequest] Missing a username");
+        } else if(request.getPassword() == null) {
+            throw new RuntimeException("[BadRequest] Missing a password");
+        }
+
+        // TODO: Generates dummy data. Replace with a real implementation.
+        User user = getDummyUser();
+        AuthToken authToken = getDummyAuthToken();
+        return new RegisterResponse(user, authToken);
     }
 }
